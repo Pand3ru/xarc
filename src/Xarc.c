2afca89abec419c5dfc25e3f57fa077aae84db27
Your Name <57118272+Pand3ru@users.noreply.github.com>
2024-09-27 16:43:01 +0200

    Add test

--- src/Xarc.c
+++ src/Xarc.c
@@ -1,29 +1,8 @@
-#include "../include/Data.h"
+#include "../include/TestGenerateDataStream.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-// Declare your GenerateDataStream function and necessary includes here
-char *GenerateDataStream(char *path, size_t *totalsize);
-
 int main() {
-  size_t totalSize; // Declare a variable to hold the total size
-  char *dataStream = GenerateDataStream(".", &totalSize); // Call the function
-
-  if (dataStream != NULL) {
-    // Successfully received a byte stream; print the total size
-
-    // Print the entire data stream in hexadecimal format
-    for (size_t i = 0; i < totalSize; i++) {
-      printf("%02X ", (unsigned char)dataStream[i]);
-    }
-    printf("Total size of the data stream: %zu bytes\n", totalSize);
-    printf("\n");
-
-    free(dataStream); // Free the allocated byte stream
-  } else {
-    // Handle error if the function returns NULL
-    printf("Failed to generate data stream.\n");
-  }
-
+  TestGenerateDataStream();
   return 0;
 }
